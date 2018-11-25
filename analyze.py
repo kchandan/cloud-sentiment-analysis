dfeb86b20df97112283e5c53a01f18776d4afcb9
Chandan Kumar <chandank@becloudready.com>
2018-11-25 17:06:13 -0500

    added updated analyze code

--- analyze.py
+++ analyze.py
@@ -1,20 +1,41 @@
-# import libs.aws
-# import libs.azure
+import libs.aws
+import libs.azure
 # import libs.gcp
-import libs.analyzer
+import libs.local
 import libs.twitter
 from textblob import TextBlob
+import json
+import pandas as pd
+import matplotlib.pyplot as plt
 
-        
+def py_analyze(df):
+    a = libs.local.analyzer()
+    a.bulk_detect_sentiment(df)
+
+def aws_analyze(df):
+    a = libs.aws.aws_comp('us-east-1')
+    a.bulk_detect_sentiment(df)
+
+def azure_analyze(df):
+    a = libs.azure.azure()
+    a.bulk_detect_sentiment(df)
+
 def analyze():
     tweets = libs.twitter.twitter()
-    search_results = tweets.tweet_batch("weed",10)
-    a = libs.analyzer.analyzer()
-    a.analyze_df(search_results)
-    
+    search_results = tweets.tweet_batch("trump",10)
+    data = []
+    df = pd.DataFrame(columns=['tweets'])
+    for num,tweet in enumerate(search_results['statuses']):
+        if tweet['user']['lang'] == 'en':
+            data.append(tweet['text'])
+    df['tweets'] = data
+    py_analyze(df)
+    aws_analyze(df)
+    azure_analyze(df)
 
 def main():
     analyze()
+    
 
 
 if __name__ == "__main__":
